4e81f8afc3c4d165356b0d1191b409634b2e423a
Fernando Maia <fermaiasoares@aol.com>
2021-12-19 23:21:34 -0300

    Tema da Aplicação

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+export default {
+  fonts: {
+    primary_400: 'Inter_400Regular',
+    primary_500: 'Inter_500Medium',
+    secondary_400: 'Archivo_400Regular',
+    secondary_500: 'Archivo_500Medium',
+    secondary_600: 'Archivo_600SemiBold',
+  },
+  colors: {
+    header: '#1b1b1f',
+    background: {
+      primary: '#f4f5f6',
+      secondary: '#ffffff'
+    },
+    text: {
+      default: '#7a7a80',
+      detail: '#aeaeb3',
+      title: '#47474d',
+    },
+    line: '#ebebf0',
+    main: {
+      default: '#dc1637',
+      ligth: '#fdedef',
+      success: '#03b252'
+    },
+    shape: {
+      default: '#e1e1e8',
+      dark: '#29292e'
+    }
+  }
+}
